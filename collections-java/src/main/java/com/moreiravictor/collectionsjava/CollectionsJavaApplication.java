60097f6c67ff9c7d766f96696095cc865b744af6
Victor Moreira <victorsmoreira@outlook.com>
2021-04-03 09:04:39 -0300

    java collections course cloned from wesleyfuchter

---
+++ collections-java/src/main/java/com/moreiravictor/collectionsjava/CollectionsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.moreiravictor.collectionsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsJavaApplication.class, args);
+	}
+
+}
